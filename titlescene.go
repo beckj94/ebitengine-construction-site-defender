278ed645b55a68646eb48ca91020088a821848d9
Jonathan Beck <jonathan.beck@adevinta.com>
2024-06-17 22:32:08 +0200

    WIP commit

---
+++ titlescene.go
@@ -0,0 +1,20 @@
+package gameModule
+
+import (
+  "errors"
+  "image"
+  _ "image/png"
+  _ "image/jpeg"
+  "os"
+  "log"
+  "time"
+
+  "bytes"
+  "io/ioutil"
+
+  // "github.com/hajimehoshi/ebiten/v2"
+  // "github.com/hajimehoshi/ebiten/v2/ebitenutil"
+  // "github.com/hajimehoshi/ebiten/v2/audio"
+  // "github.com/hajimehoshi/ebiten/v2/audio/wav"
+  // "github.com/hajimehoshi/ebiten/v2/inpututil"
+)
